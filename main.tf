916c64850242a5e00bc05a17a1adfbc35a51086e
Clairo Dorneles <clairo@clairodorneles.cloud>
2025-03-08 14:31:49 +0100

    fixed fmt

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_shared_image_gallery" "this" {
   resource_group_name = var.resource_group_name
   location            = var.location
   description         = var.description
-  tags                =  var.tags
+  tags                = var.tags
 }
 
 
