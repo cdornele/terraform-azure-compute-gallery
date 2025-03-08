043425821fd6db79fad98e86945a876419d65673
Clairo Dorneles <clairo@clairodorneles.cloud>
2025-03-08 13:55:46 +0100

    fixed provider

--- version.tf
+++ version.tf
@@ -8,10 +8,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.10"
     }
-    azurecaf = {
-      source  = "aztfmod/azurecaf"
-      version = "2.0.0-preview3"
-    }
   }
 }
 
