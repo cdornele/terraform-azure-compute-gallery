5823bcece3fcaed080dbe5ff6d8dbb7627c6cf05
Clairo Dorneles <clairo@clairodorneles.cloud>
2025-03-10 10:35:59 +0100

    fixed naming and main code

--- d-naming.tf
+++ d-naming.tf
@@ -5,8 +5,8 @@
 data "azurecaf_name" "shared_image_gallery" {
   name          = var.shared_image_gallery_name
   resource_type = "azurerm_shared_image_gallery"
-  prefixes      = lower(var.prefixes)
-  suffixes      = lower(var.suffixes)
+  prefixes      = var.prefixes
+  suffixes      = var.suffixes
   use_slug      = true
   clean_input   = true
   separator     = "-"
